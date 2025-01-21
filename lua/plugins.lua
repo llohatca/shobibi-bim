265afd57c30c7d00ad53ab6bb98d7b1e47d819e7
ReStranger <restranger@disroot.org>
2025-01-22 01:57:12 +0300

    feet(dap): Drop dap and mini animate configuration
    - Preparing for refactoring
    
    Signed-off-by: ReStranger <restranger@disroot.org>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -77,41 +77,6 @@ require("lazy").setup {
         },
     },
     {
-        "echasnovski/mini.animate",
-        event = "VeryLazy",
-        dependencies = "echasnovski/mini.nvim",
-        config = function()
-            require "configs.mini-animate"
-        end,
-    },
-    {
-        "rcarriga/nvim-dap-ui",
-        event = "VeryLazy",
-        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-        config = function()
-            local dap, dapui = require "dap", require "dapui"
-            dap.listeners.before.attach.dapui_config = function()
-                dapui.open()
-            end
-            dap.listeners.before.launch.dapui_config = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated.dapui_config = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited.dapui_config = function()
-                dapui.close()
-            end
-        end,
-    },
-    {
-        "mfussenegger/nvim-dap",
-        event = "VeryLazy",
-        config = function()
-            require "configs.dap"
-        end,
-    },
-    {
         "nvimdev/dashboard-nvim",
         event = "VimEnter",
         config = function()
